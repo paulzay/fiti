cb6cc85ccc518fcfe1d03f5ee7f3ab3c08d39914
Paul Ogolla <paulotieno2@gmail.com>
2024-10-29 20:49:38 +0300

    add more routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from 'react';
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './App.css'
 import Login from './pages/Login';
-import Home from './pages/Home';
+import Homepage from './pages/Homepage';
+import Signup from './pages/Signup';
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
 
-export const queryClient = new QueryClient();
 function App() {
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path='/home' element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
