7b82d92d600018f6087e56323eeb446c13e1cc34
Paul Ogolla <paulotieno2@gmail.com>
2024-10-29 20:50:49 +0300

    login and signup pages + cookie functionality

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,56 +2,65 @@ import { useNavigate } from "react-router-dom";
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
+// import { User } from '@/types'
+// import { useState } from 'react'
 import { Label } from "@/components/ui/label"
-import { setCookie } from "@/utils/methods";
+// import {
+//   useQuery,
+//   useMutation,
+//   useQueryClient,
+//   QueryClient,
+//   QueryClientProvider,
+// } from 'react-query'
 
-
-function Login() {
+function Signup() {
   const navigate = useNavigate()
-
-  const login = async (email: string, password: string) => {
-    const response = await fetch('http://localhost:8000/api/v1/login', {
+  const signup = async (name: string, email: string, password: string) => {
+    const response = await fetch('http://localhost:8000/api/v1/signup', {
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ name, email, password }),
 
     })
-    console.log(response)
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     } else {
       const json = await response.json();
-      setCookie('auth', json.token, 1)
-      navigate("/dashboard")
+      console.log(json)
+      navigate("/login")
     }
   }
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Login</CardTitle>
+        <CardTitle>Signup</CardTitle>
       </CardHeader>
       <CardContent>
         <form onSubmit={(e) => {
           e.preventDefault()
           const formData = new FormData(e.currentTarget)
-          login(formData.get("email") as string, formData.get("password") as string)
+          signup(formData.get("name") as string, formData.get("email") as string, formData.get('password') as string)
         }} className="space-y-4">
+          <div>
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" name="name" required />
+          </div>
           <div>
             <Label htmlFor="email">Email</Label>
             <Input id="email" name="email" type="email" required />
           </div>
           <div>
             <Label htmlFor="password">Password</Label>
-            <Input id="password" name="password" type="password" required />
+            <Input type="password" id="password" name="password" required />
           </div>
-          <Button type="submit">Login</Button>
+          <Button type="submit">Signup</Button>
         </form>
       </CardContent>
     </Card>
   )
 }
 
-export default Login
+export default Signup;
