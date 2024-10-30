25d94968b50b7d0623b6d811b780379fcafa549a
Paul Ogolla <paulotieno2@gmail.com>
2024-10-30 14:46:54 +0300

    revamp signup and login pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,56 +1,49 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from "@/components/ui/label"
-import { setCookie } from "@/utils/methods";
+import { useAuth } from '../utils/methods';
 
 
 function Login() {
-  const navigate = useNavigate()
+  const { login } = useAuth();
 
-  const login = async (email: string, password: string) => {
-    const response = await fetch('http://localhost:8000/api/v1/login', {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-
-    })
-    console.log(response)
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    } else {
-      const json = await response.json();
-      setCookie('auth', json.token, 1)
-      navigate("/dashboard")
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      login(email, password)
+    }
+    catch (error) {
+      console.error(error)
     }
   }
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Login</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          const formData = new FormData(e.currentTarget)
-          login(formData.get("email") as string, formData.get("password") as string)
-        }} className="space-y-4">
-          <div>
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" name="email" type="email" required />
-          </div>
-          <div>
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" name="password" type="password" required />
-          </div>
-          <Button type="submit">Login</Button>
-        </form>
-      </CardContent>
-    </Card>
+    <div className="py-12 md:py-24 lg:py-32 xl:py-48">
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            const formData = new FormData(e.currentTarget)
+            handleLogin(formData.get("email") as string, formData.get("password") as string)
+          }} className="space-y-4">
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" name="email" type="email" required />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" name="password" type="password" required />
+            </div>
+            <Button type="submit">Login</Button>
+            <p>Don't have an account? <Link to="/signup">Signup</Link></p>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
