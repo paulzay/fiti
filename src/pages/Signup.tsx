55b42564a9ca141edb2e1a24eaa56d2b3e372fd8
Paul Ogolla <paulotieno2@gmail.com>
2024-10-30 14:46:54 +0300

    revamp signup and login pages

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,17 +1,8 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
-// import { User } from '@/types'
-// import { useState } from 'react'
 import { Label } from "@/components/ui/label"
-// import {
-//   useQuery,
-//   useMutation,
-//   useQueryClient,
-//   QueryClient,
-//   QueryClientProvider,
-// } from 'react-query'
 
 function Signup() {
   const navigate = useNavigate()
@@ -27,39 +18,40 @@ function Signup() {
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     } else {
-      const json = await response.json();
-      console.log(json)
       navigate("/login")
     }
   }
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Signup</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          const formData = new FormData(e.currentTarget)
-          signup(formData.get("name") as string, formData.get("email") as string, formData.get('password') as string)
-        }} className="space-y-4">
-          <div>
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" name="name" required />
-          </div>
-          <div>
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" name="email" type="email" required />
-          </div>
-          <div>
-            <Label htmlFor="password">Password</Label>
-            <Input type="password" id="password" name="password" required />
-          </div>
-          <Button type="submit">Signup</Button>
-        </form>
-      </CardContent>
-    </Card>
+    <div className="py-12 md:py-24 lg:py-32 xl:py-48">
+      <Card>
+        <CardHeader>
+          <CardTitle>Signup</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            const formData = new FormData(e.currentTarget)
+            signup(formData.get("name") as string, formData.get("email") as string, formData.get('password') as string)
+          }} className="space-y-4">
+            <div>
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" name="name" required />
+            </div>
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" name="email" type="email" required />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" name="password" required />
+            </div>
+            <Button type="submit">Signup</Button>
+            <p>Already have an account? <Link to="/login">Login</Link></p>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
